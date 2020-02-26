ebe98df2c68fc6e21af279671525b399ae8c4deb
Ghabe Bossin <ghabe.bossin@gmail.com>
2020-02-26 13:58:27 -0800

    variable change

--- app/move.rb
+++ app/move.rb
@@ -1,15 +1,15 @@
 # TODO: Implement your logic here!
 # View docs at https://docs.battlesnake.com/snake-api for example payloads.
-def move(board)
+def move(data)
   directions = [:up, :down, :left, :right]
-  safe_directions = avoid_self(board, directions)
-  safer_directions = avoid_wall(board, safe_directions)
+  safe_directions = avoid_self(data, directions)
+  safer_directions = avoid_wall(data, safe_directions)
   move = safer_directions.sample
   { move: move }
 end
 
-def avoid_self(board, directions)
-  body = board[:you][:body]
+def avoid_self(data, directions)
+  body = data[:you][:body]
   head = body[0]
   x = head[:x]
   y = head[:y]
@@ -32,8 +32,8 @@ def avoid_self(board, directions)
   directions
 end
 
-def avoid_wall(board, directions)
-  body = board[:you][:body]
+def avoid_wall(data, directions)
+  body = data[:you][:body]
   head = body[0]
   x = head[:x]
   y = head[:y]
@@ -44,10 +44,10 @@ def avoid_wall(board, directions)
   if up[:y] == -1
     directions.delete(:up)
   end
-  if down[:y] == board[:board][:height]
+  if down[:y] == data[:board][:height]
     directions.delete(:down)
   end
-  if right[:x] == board[:board][:width]
+  if right[:x] == data[:board][:width]
     directions.delete(:right)
   end
   if left[:x] == 0
