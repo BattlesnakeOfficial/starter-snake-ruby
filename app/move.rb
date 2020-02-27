fbcaed31c8e7db7cb1bd3c82dd64833b208dc051
Ghabe Bossin <ghabe.bossin@gmail.com>
2020-02-26 20:03:07 -0800

    use readable-datamethods

--- app/move.rb
+++ app/move.rb
@@ -1,42 +1,46 @@
 # TODO: Implement your logic here!
 # View docs at https://docs.battlesnake.com/snake-api for example payloads.
 
-def readable_snek_data(data)
-  snek = data[:you]
-  snek_body = snek[:body]
-  snek_data = {
-    snek: snek,
-    health: snek[:health],
-    body: snek_body,
-    head: snek_body.first,
-    head_x: snek_body.first[:x],
-    head_y: snek_body.first[:y],
-    tail: snek_body.last
-    # head_position: {
-    #   x: data[:you][:body][0][:x],
-    #   y: data[:you][:body][0][:y]
-    # }
+# This is used to output all the data for our snake, Letty.
+# Add needed letty info here, access it by calling method.
+def readable_letty_data(data)
+  letty = data[:you]
+  letty_body = letty[:body]
+  letty_data = {
+    snek: letty,
+    health: letty[:health],
+    body: letty_body,
+    head: letty_body.first,
+    head_x: letty_body.first[:x],
+    head_y: letty_body.first[:y],
+    tail: letty_body.last,
+    tail_x: letty_body.last[:x],
+    tail_y: letty_body.last[:y]
   }
-  return snek_data
+  return letty_data
 end
 
+# This is used to output all the game data that isn't our snake.
+# Add needed game data here, access it by calling method.
 def readable_board_data(data)
-  board = {
-    board: data[:board],
-    width: data[:board][:width],
-    height: data[:board][:height],
+  board = data[:board]
+  board_data = {
+    board: board,
+    width: board[:width],
+    height: board[:height],
+    food: board[:food],
+    snakes: board[:snakes]
   }
-  return board
+  return board_data
 end
 
 def move(data)
-  snek = readable_snek_data(data)
-  board = readable_board_data(data)
+  letty = readable_letty_data(data)
   directions = [:up, :down, :left, :right]
   safe_directions = avoid_obstacles(data, directions)
   move = safe_directions.sample
 
-  if (snek[:health] >= 60)
+  if (letty[:health] >= 70)
     move = chase_tail(data, safe_directions).sample
     { move: move }
   else
@@ -45,30 +49,28 @@ def move(data)
 end
 
 def avoid_obstacles(data, directions)
-  snek = readable_snek_data(data)
+  letty = readable_letty_data(data)
   board = readable_board_data(data)
-  body = snek[:body]
-  head = body.first
-  snakes = data[:board][:snakes]
 
-  x = head[:x]
-  y = head[:y]
-  up = {:x=>x, :y=>y-1}
-  down = {:x=>x, :y=>y+1}
-  left = {:x=>x-1, :y=>y}
-  right = {:x=>x+1, :y=>y}
+  head_x = letty[:head_x]
+  head_y = letty[:head_y]
 
-  snakes.each do |snake|
-    if body.include?(up) || snake[:body].include?(up) || up[:y] == -1
+  up = { x: head_x, y: head_y - 1 }
+  down = { x: head_x, y: head_y + 1 }
+  left = { x: head_x - 1, y: head_y }
+  right = { x: head_x + 1, y: head_y }
+
+  board[:snakes].each do |snake|
+    if letty[:body].include?(up) || snake[:body].include?(up) || up[:y] == -1
       directions.delete(:up)
     end
-    if body.include?(down) || snake[:body].include?(down) || down[:y] ==data[:board][:height]
+    if letty[:body].include?(down) || snake[:body].include?(down) || down[:y] == board[:height]
       directions.delete(:down)
     end
-    if body.include?(left) || snake[:body].include?(left) || left[:x] == -1
+    if letty[:body].include?(left) || snake[:body].include?(left) || left[:x] == -1
       directions.delete(:left)
     end
-    if body.include?(right) || snake[:body].include?(right) || right[:x] == data[:board][:width]
+    if letty[:body].include?(right) || snake[:body].include?(right) || right[:x] == board[:width]
       directions.delete(:right)
     end
   end
@@ -77,32 +79,27 @@ def avoid_obstacles(data, directions)
 end
 
 def chase_tail(data, directions)
-  snek = readable_snek_data(data)
-  board = readable_board_data(data)
-  # head_x = snek[:head][:x]
-  tail_x = snek[:tail][:x]
-  head_y = snek[:head][:y]
-  tail_y = snek[:tail][:y]
+  letty = readable_letty_data(data)
 
-  if snek[:head_x] < tail_x and directions.include?(:left)
+  if letty[:head_x] < letty[:tail_x] and directions.include?(:left)
     directions.delete(:left)
     directions.push(:right)
     directions = avoid_obstacles(data, directions)
   end
 
-  if snek[:head_x] > tail_x and directions.include?(:right)
+  if letty[:head_x] > letty[:tail_x] and directions.include?(:right)
     directions.delete(:right)
     directions.push(:left)
     directions = avoid_obstacles(data, directions)
   end
 
-  if head_y < tail_y and directions.include?(:up)
+  if letty[:head_y] < letty[:tail_y] and directions.include?(:up)
     directions.delete(:up)
     directions.push(:down)
     directions = avoid_obstacles(data, directions)
   end
 
-  if head_y > tail_y and directions.include?(:down)
+  if letty[:head_y] > letty[:tail_y] and directions.include?(:down)
     directions.delete(:down)
     directions.push(:up)
     directions = avoid_obstacles(data, directions)
