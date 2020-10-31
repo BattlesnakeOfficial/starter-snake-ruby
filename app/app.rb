bb6e44b6b877aafc948d3bd06d752ee8d8c47af1
Nettogrof <carl.lajeunesse@gmail.com>
2020-10-31 18:28:03 -0400

    Change to Api Version 1 and Adjust the README

--- app/app.rb
+++ app/app.rb
@@ -5,35 +5,33 @@ require './app/util'
 require './app/move'
 
 use Rack::PostBodyContentTypeParser
-
-# If you open your Battlesnake URL in a browser you should see this message.
+# This function is called when you register your Battlesnake on play.battlesnake.com
+# It controls your Battlesnake appearance and author permissions.
+# TIP: If you open your Battlesnake URL in browser you should see this data
 get '/' do
-  "Your Battlesnake is alive!\n"
-end
-
-# The Battlesnake engine calls this function to make sure your Battlesnake is working.
-post '/ping' do
-  "pong\n"
-end
-
-# This function is called everytime your Battlesnake is entered into a game.
-# request contains information about the game that is about to be played.
-# TODO: Use this function to decide how your Battlesnake is going to look on the board.
-post '/start' do
-  request = underscore(env['rack.request.form_hash'])
-  puts "START"
-  content_type :json
-  
   appearance = {
-    color: "#888888",
-    head_type: "regular",
-    tail_type: "regular",
+    apiversion: "1",        
+    author: "",           # TODO: Your Battlesnake Username
+    color: "#888888",     # TODO: Personalize
+    head_type: "default", # TODO: Personalize
+    tail_type: "default", # TODO: Personalize
   }
 
   camelcase(appearance).to_json
 end
 
-# This function is called on every turn of a game. It's how your Battlesnake decides where to move.
+# This function is called everytime your snake is entered into a game.
+# rack.request.form_hash contains information about the game that's about to be played.
+# TODO: Use this function to decide how your snake is going to look on the board.
+post '/start' do
+  request = underscore(env['rack.request.form_hash'])
+  puts "START"
+  "OK\n"
+end
+
+# This function is called on every turn of a game. It's how your snake decides where to move.
+# Valid moves are "up", "down", "left", or "right".
+# TODO: Use the information in rack.request.form_hash to decide your next move.
 post '/move' do
   request = underscore(env['rack.request.form_hash'])
 
@@ -47,5 +45,5 @@ end
 # It's purely for informational purposes, you don't have to make any decisions here.
 post '/end' do
   puts "END"
-  "ok\n"
+  "OK\n"
 end
