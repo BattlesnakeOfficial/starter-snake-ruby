68b1a35e71b91aeb6c45c0325fc066cb45aa209d
Jordan Sandberg <jordansandberg1@gmail.com>
2022-06-16 18:08:39 -0700

    first iteration of graph code (untested)

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 
 ruby '2.7.5'
 gem 'activesupport'
+gem 'gastar'
 gem 'json'
 gem 'rack-contrib'
 gem 'rubocop'
---
+++ app/graph/node.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'gastar'
+
+# Implement the abstract node class.
+# Note: All attributes below are custom for this implementation and none are
+# needed nor used by the actual AStar seach algorithm. They're my domain atts.
+module Graph
+  class Node < AStarNode
+    attr_reader :type, :x, :y
+
+    def initialize(type, x, y)
+      super
+      @type = type
+      @x = x
+      @y = y
+    end
+
+    def move_cost(_other)
+      case @type
+      when :food then 0
+      when :board then 10
+      end
+    end
+
+    def to_s
+      @type
+    end
+  end
+end
